0d7570c858658119094a34b6c37f1c8d9d4b5cf6
AdletTop1234 <adlet2007094@gmail.com>
2026-09-19 11:56:31 +0500

    feat(mergesort):create mergesort and merge in O(n) time.

---
+++ mergeSort.java
@@ -0,0 +1,49 @@
+public class mergeSort {
+    public static void sort(int[] arr){
+        if (arr == null or arr <= 1){
+            return;
+        }
+
+        int[] temp = new int[arr.length];
+
+        mergesort(arr,temp,0,arr.length - 1)
+    }
+
+    public static void mergesort(int[] arr, int[] temp, int left, int right){
+        int mid = left + (right - left) / 2;
+
+        mergesort(arr, temp, left, mid);
+        mergesort(arr, temp, mid + 1, right);
+
+        if (arr[mid] <= arr[mid + 1]){
+            return;
+        }
+
+        merge(arr, temp, left, mid, right)
+    }
+
+    public static void merge(int[] arr,int[] temp,int left,int mid,int right){
+        for(int i = left, i <= right, i++){
+            temp[i] = arr[i];
+        }
+
+        int i = left;
+        int j = mid + 1;
+        int l = left;
+
+        while(i <= mid && j <=right) {
+            if (temp[i] <= temp[j]) {
+                arr[k++] = temp[i++]
+            } else {
+                arr[k++] = temp[j++]
+            }
+        }
+        while(i <= mid){
+            arr[k++] = temp[j++];
+        }
+    }
+
+    public static void insertionSort{
+
+    }
+}
